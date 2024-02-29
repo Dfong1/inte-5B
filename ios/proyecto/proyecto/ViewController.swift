d19b78fd52e4d44e704b4ba0361eedbfe3946fad
Daniel Fong <danifongm@gmail.com>
2024-02-29 13:45:14 -0600

    Segundo commit

---
+++ ios/proyecto/proyecto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proyecto
+//
+//  Created by Mac14 on 29/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
