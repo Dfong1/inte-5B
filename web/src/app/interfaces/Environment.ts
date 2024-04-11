df4e4782fc07537ff0f467a2509e0960eb7066c3
Daniel Fong <danifongm@gmail.com>
2024-04-11 13:28:09 -0600

    ahora si

--- web/src/app/interfaces/Environment.ts
+++ web/src/app/interfaces/Environment.ts
@@ -1,10 +1,5 @@
 import Echo from "laravel-echo"
-
-<<<<<<< HEAD
-export const api = 'http://127.0.0.1:8000' 
-=======
 export const api = 'http://192.168.253.180:8000'
->>>>>>> 6f5086645c23430ba6c6a7f7f674b302238eb2e1
 export const echo: Echo = new Echo({
     broadcaster:'pusher',
     key:'123',
